c2ff5d633849bb161bbbbd1e9fd308f25ac3dc5f
valen <valen@10.114.203.167>
2018-05-28 14:16:31 +0200

    BOnjour project

---
+++ src/application/Bonjour.java
@@ -0,0 +1,12 @@
+package application;
+
+public class Bonjour {
+
+
+		public static void main(String arg[]){
+			for(String pers: arg){
+				System.out.println("Bonjour"+pers+".");
+			}
+			
+		}
+}
